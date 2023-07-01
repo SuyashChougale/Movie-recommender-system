b89622335c8add9211477fd85bd3db1e8920d434
SuyashChougale <135426582+SuyashChougale@users.noreply.github.com>
2023-07-01 10:49:30 +0530

    cleaned and formatted  app.py

--- app.py
+++ app.py
@@ -2,14 +2,10 @@ import streamlit as st
 from model import combo,recommend 
 
 st.title("movie recommendetion system")
-# a = recommend()
-# print(combo)
-
-
 
 option = st.selectbox('How would you like to be contacted?',combo['title']) 
 if st.button('Recommend'):
-    titles,poster = recommend(option)
+    titles = recommend(option)
     for i in range(0,5):
         st.write(titles[i])
     
